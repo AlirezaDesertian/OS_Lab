bdae2aec7c80bab040f865494408556b98845826
Alireza Desertian <alirezadesertian@gmail.com>
2026-01-06 20:03:41 +0330

    add project9 skeleton for OS lab

---
+++ project9/README.md
@@ -0,0 +1,5 @@
+# Project 9 - OS Lab
+
+Dining Philosophers Problem  
+Language: C  
+Using pthread and semaphore
---
+++ project9/src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Project 9 - Dining Philosophers\n");
+    return 0;
+}
